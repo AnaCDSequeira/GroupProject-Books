4ef4a837479da8c563ac9dd6893c3298d8b397bd
Joao Santos <joao.santosjoaos@DESKTOP-RG88VTM MINGW64 ~$ pwd/c/Users/joaosjoaos@DESKTOP-RG88VTM MINGW64 ~$ ls AppData/'Application Data'@ Contacts/ Cookies@'Definies Locais'@ Desktop/ Documents/ Downloads/ Favorites/ IntelGraphicsProfiles/ Links/'Menu Iniciar'@ Modelos@ Music/ NTUSER.DAT NTUSER.DAT{1caa2eaa-0672-11ed-96d9-9cb6d0c318c4}.TM.blf NTUSER.DAT{1caa2eaa-0672-11ed-96d9-9cb6d0c318c4}.TMContainer00000000000000000001.regtrans-ms NTUSER.DAT{1caa2eaa-0672-11ed-96d9-9cb6d0c318c4}.TMContainer00000000000000000002.regtrans-ms NetHood@ OneDrive/'Os Meus Documentos'@ Pictures/ PrintHood@ Recent@'Saved Games'/ Searches/ SendTo@ Videos/ ntuser.dat.LOG1 ntuser.dat.LOG2 ntuser.inijoaos@DESKTOP-RG88VTM MINGW64 ~$joaos@DESKTOP-RG88VTM MINGW64 ~$ cd Desktopjoaos@DESKTOP-RG88VTM MINGW64 ~/Desktop$ cd MyProjectsjoaos@DESKTOP-RG88VTM MINGW64 ~/Desktop/MyProjects$ cd clone https://github.com/jpbs18/GuessNumber.gitbash: cd: too many argumentsjoaos@DESKTOP-RG88VTM MINGW64 ~/Desktop/MyProjects$ git config --global user.name Joao>
2022-11-07 16:25:59 +0000

    Profile page finished

--- src/pages/LoginPage/Login.jsx
+++ src/pages/LoginPage/Login.jsx
@@ -43,6 +43,7 @@ const Login = (props) => {
       }
     }
     loginUser();
+    
   }
 
   return (
--- src/pages/Profile/ProfilePage.jsx
+++ src/pages/Profile/ProfilePage.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
 
-function ProfilePage(props) {
+const ProfilePage = (props) => {
+
   const { loggedIn } = props;
-  const [user, setUser] = useState();
+  const [user, setUser] = useState({});
+  const [isLoaded, setIsLoaded] = useState(false)
 
   const myHeaders = new Headers();
 
-  myHeaders.append("Authorization", localStorage.getItem(loggedIn.email));
+  console.log(loggedIn.email)
+  myHeaders.append("Authorization", localStorage.getItem(loggedIn.email))
 
   var requestOptions = {
     method: "GET",
@@ -15,66 +18,148 @@ function ProfilePage(props) {
   };
 
   useEffect(() => {
+
     async function getUser() {
+
       const response = await fetch(
         "https://cors-anywhere.herokuapp.com/ancient-temple-61124.herokuapp.com/api/user/profile",
         requestOptions
       );
+
       const json = await response.json();
-      setUser(json);
-      console.log(user);
+      const data = json.data 
+      setUser(data);
     }
     getUser();
+
   }, []);
+
+  console.log(user)
+
+  function handleSubmitUser(e){
+    e.preventDefault();
+
+    const userDetails = [e.target.userName.value, e.target.userEmail.value, e.target.userPassword.value, e.target.userImage.value] 
+
+
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", localStorage.getItem(loggedIn.email));
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+      "email": userDetails[1],
+      "password": userDetails[2],
+      "name": userDetails[0],
+      "profile_picture": userDetails[3]
+    });
+
+    const requestOptions = {
+      method: 'PUT',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+
+      async function updateUser(){
+
+        const response = await fetch("https://cors-anywhere.herokuapp.com/ancient-temple-61124.herokuapp.com/api/user/profile", requestOptions)
+        const json = await response.json();
+        console.log(json)
+      }
+  
+      updateUser()
+
+    console.log(userDetails)
+  }
+
+
+  function handleSubmitBook(e){
+    e.preventDefault();
+
+    const bookDetails = [e.target.bookTitle.value, e.target.bookYear.value, e.target.bookDescription.value, e.target.bookCover.value]
+    console.log(bookDetails)
+
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", localStorage.getItem(loggedIn.email));
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+      "title": bookDetails[0],
+      "description": bookDetails[2],
+      "year": parseInt(bookDetails[1]),
+      "book_cover": bookDetails[3]
+    });
+
+    const requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+
+
+    async function addBook(){
+
+      const response = await fetch("https://cors-anywhere.herokuapp.com/ancient-temple-61124.herokuapp.com/api/book/", requestOptions)
+      const json = await response.json();
+      console.log(json)
+    }
+
+    addBook();
+
+    console.log(bookDetails)
+}
+  console.log(user)
 
   return (
     <>
-      <div>
-        <img src={user.data.profile_picture} alt={user.data.name} />
+    <div>
+        <img src={user.profile_picture}/>
       </div>
       <div>
-        <form action="">
+        <form onSubmit={handleSubmitUser}>
           <label>
             Name
-            <input type="text" />
+            <input type="text" name="userName" />
           </label>
           <label>
             Email
-            <input type="email" />
+            <input type="email" name="userEmail" />
           </label>
           <label>
             Password
-            <input type="password" />
+            <input type="password" name="userPassword" />
           </label>
           <label>
             Image
-            <input type="url" />
+            <input type="url" name="userImage" />
           </label>
-          <button>Send</button>
+          <button type="submit">Send</button>
         </form>
       </div>
       <div>
-        <form action="">
+        <form onSubmit={handleSubmitBook}>
           <label>
             Title
-            <input type="text" />
+            <input type="text" name="bookTitle" />
           </label>
           <label>
             Year
-            <input type="text" />
+            <input type="text" name="bookYear" />
           </label>
           <label>
             Description
-            <textarea rows="8" cols="50" />
+            <textarea rows="8" cols="50" name="bookDescription" />
           </label>
           <label>
-            Book cover <input type="url" />
+            Book cover <input type="url" name="bookCover" />
           </label>
-          <button>Send</button>
+          <button type="submit">Send</button>
         </form>
       </div>
     </>
-  );
+  )
+  
 }
 
 export { ProfilePage };
